8108d2b96df36c4e4c5d1a42589a1a4bdd1756fe
Lakshmi Kuram Galla <lakshmi.kuramgalla8@gmail.com>
2025-12-12 12:39:48 +0530

    Add files via upload

---
+++ fahrenheit_to_celcius.py
@@ -0,0 +1,7 @@
+#program to convert Fahrenheit to Celcius
+#1.Fahrenheit value 
+f=95.3
+#2.Celcius value:c=(f-32)+5/9
+c=(f-32)+5/9
+#3.print result and test it
+print(f"celcius value of Fahrenheit {f} is {c}")

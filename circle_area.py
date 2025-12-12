70e292fa9f6e84d9eff6e8505298ef4eadab5375
Lakshmi Kuram Galla <lakshmi.kuramgalla8@gmail.com>
2025-12-12 12:42:54 +0530

    Add files via upload

---
+++ circle_area.py
@@ -0,0 +1,8 @@
+#program to find area of a circle : pi*r*r
+#1.radius of a circle 
+r = 15
+#2.find area
+area = 3.14*15*15
+
+#3.print result and test 
+print(f"area of a circle with radius {r} is {area}")

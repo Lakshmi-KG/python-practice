9b75da2a63cec4e098d2ad3d7290b147752e117a
Lakshmi Kuram Galla <lakshmi.kuramgalla8@gmail.com>
2025-12-12 12:42:26 +0530

    Add files via upload

---
+++ area_of_rectangle.py
@@ -0,0 +1,8 @@
+#program to find a area of rectangle is
+# 1.take side1 and side2 values
+side1 = int(input("enter a value"))
+side2 = int(input("enter b value"))
+# find area 
+area = side1*side2
+#print result and tast it
+print(f"area of a rectangle with {side1},{side2} is {area}")

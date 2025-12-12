030d3d38f9dfe8374638d38eafb840033c6438e7
Lakshmi Kuram Galla <lakshmi.kuramgalla8@gmail.com>
2025-12-12 12:39:11 +0530

    Add files via upload

---
+++ sumof2numbers.py
@@ -0,0 +1,8 @@
+#program to find sum of numbers
+#1.take 2 numbers
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+#find sum
+c=a+b
+#print result and test it
+print(f"sum of {a},{b} is {c}")
